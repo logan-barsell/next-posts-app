c93023b8dbd429d3461655de52453c77bf560ca1
Logan Barsell <loganjbars@gmail.com>
2024-10-28 23:31:40 -0700

    nextjs img optimization + metadata

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,6 +1,15 @@
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+export async function generateMetadata() {
+  const posts = await getPosts();
+  const numPosts = posts.length;
+  return {
+    title: `See our ${numPosts} posts.`,
+    description: 'Browse all our posts.',
+  };
+}
+
 export default async function FeedPage() {
   const posts = await getPosts();
   return (
--- components/header.js
+++ components/header.js
@@ -1,19 +1,31 @@
 import logo from '@/assets/logo.png';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Header() {
   return (
-    <header id="main-header">
-      <Link href="/">
-        <img src={logo.src} alt="Mobile phone with posts feed on it" />
+    <header id='main-header'>
+      <Link href='/'>
+        <Image
+          height={100}
+          width={100}
+          src={logo}
+          priority
+          alt='Mobile phone with posts feed on it'
+        />
       </Link>
       <nav>
         <ul>
           <li>
-            <Link href="/feed">Feed</Link>
+            <Link href='/feed'>Feed</Link>
           </li>
           <li>
-            <Link className='cta-link' href="/new-post">New Post</Link>
+            <Link
+              className='cta-link'
+              href='/new-post'
+            >
+              New Post
+            </Link>
           </li>
         </ul>
       </nav>
